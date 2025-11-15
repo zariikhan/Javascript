1521abe23803400c51171670c79600901ce203ae
zariikhan <zaryabkhan18578@gmail.com>
2025-11-14 23:54:27 -0800

    variables

---
+++ basics/01-variables.js
@@ -0,0 +1,26 @@
+let name = "zaryab khan";
+var number = 123456789;
+const email = "zaryab@1234545667@gmail.com";
+
+country = "pakistan";
+
+console.table([name, number, email, country]);
+
+// Create three variables using var, let, and const.
+// Then console.log all three values in one line.
+
+let district = "dir lower";
+var tehsil = "adenzai";
+const postalCode = 1234;
+console.log(district,tehsil,postalCode)
+
+// Write code that shows the difference between var (function scope) and let (block scope).
+if (true) {
+    var rollNo = 23;
+}
+console.log(rollNo)
+
+if (true) {
+    let fullName = "jawad";
+}
+console.log(fullName)
