e20abd06dc670348f0756695f7f372b5c84289bc
zariikhan <zaryabkhan18578@gmail.com>
2025-11-16 23:43:49 -0800

    datatypes

---
+++ basics/02-datatypes.js
@@ -0,0 +1,55 @@
+/*datatypes
+ 1) Primitive datatypes
+ Number
+ String
+ Boolean => true/false
+ null
+ undefined
+ Symbol => uniqueness
+ BigInt => write n after the number
+
+ 2) Non Primitive datatypes
+ Object
+ array*/
+
+let name = "ali";
+console.log(typeof name);
+
+let age = 20;
+console.log(typeof age);
+
+let marks = null;
+console.log(typeof marks);
+
+let score = 0;
+console.log(typeof score);
+
+let fatherName;
+console.log(typeof fatherName);
+
+let isloggedIn = true;
+console.log(typeof isloggedIn);
+
+let obj = {
+  name: "zaryab",
+  age: 23,
+};
+console.log(typeof obj);
+
+let arr = [1, 2, 3, 4, 5];
+
+let ages = [];
+console.log(typeof ages);
+
+let output = {};
+console.log(typeof output);
+
+let BigInt = 12345678909866n;
+console.log(typeof BigInt);
+
+let value = 23;
+let stud = {
+  name: "khan",
+};
+console.log(value);
+console.log(stud);
