0f4244b5491809e2c068959233a8cfe9e1a05e9c
zariikhan <zaryabkhan18578@gmail.com>
2025-11-26 13:08:39 +0500

    number and math

---
+++ basics/06_number and math.js
@@ -0,0 +1,49 @@
+const score = 100;
+// console.log(score);
+
+const balance = new Number(1000);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toString().charAt(0));
+
+// console.log(balance.toFixed(3));
+// console.log(balance.toFixed(1));
+// console.log(balance.toPrecision(1));
+
+const value = 456.234;
+// console.log(value.toPrecision(1));
+
+const thousand = 123000000;
+// console.log(thousand.toLocaleString());
+// console.log(thousand.toExponential());
+
+// math
+// console.log(Math.abs(-10));
+// console.log(Math.round(3.5));
+// console.log(Math.round(3.2));
+// console.log(Math.ceil(3.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.max(1, 2, 3, 4));
+// console.log(Math.min(1, 2, 3, 4));
+// console.log(Math.pow(2, 5));
+// console.log(2**5)
+// console.log(Math.sqrt(144));
+
+// console.log(Math.random());
+// console.log(Math.random() * 10);
+// console.log(Math.random() * 10 + 1);
+// console.log(Math.floor(Math.random() * 8 + 1));
+
+const min = 10;
+const max = 20;
+
+// console.log(Math.floor(Math.random() * (max - min)) + min);
+
+// console.log(parseInt("123abc"));
+// console.log(parseInt("5675fgfgfg"));
+// console.log(parseInt(123.67));
+
+
+
+
