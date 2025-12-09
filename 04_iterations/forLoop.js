b3c5cdc13b0cbb504cfdec6d2354bfc7c38c54a2
zariikhan <zaryabkhan18578@gmail.com>
2025-12-10 00:20:26 +0500

    for loop

--- 04_iterations/forLoop.js
+++ 04_iterations/forLoop.js
@@ -31,18 +31,64 @@ for (let i = 0; i <= 10; i++) {
 for (let i = 0; i <= 10; i++) {
   const element = i;
   if (element == 6) {
-    console.log("final number is", element);
+    // console.log("final number is", element);
     break;
   }
-  console.log(i);
+  // console.log(i);
 }
 
 // continue
 for (let i = 0; i <= 10; i++) {
   const element = i;
   if (element == 6) {
-    console.log("final number is", element + " but continue you work");
+    // console.log("final number is", element + " but continue you work");
     continue;
   }
-  console.log(i);
+  // console.log(i);
+}
+
+// Print numbers from 1 to 10
+for (let i = 0; i <= 10; i++) {
+  const element = i;
+  // console.log(element);
+}
+
+// Print even numbers from 1 to 20
+for (let i = 1; i <= 20; i++) {
+  // console.log(`numbers is ${i}`);
+  if (i % 2 == 0) {
+    // console.log(`even numbers are ${i}`);
+  }
+}
+
+// Print the sum of numbers from 1 to 10.
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+  sum += i;
+}
+// console.log(sum);
+
+// Print the multiplication table of 5 from 1 to 10.
+for (let i = 5; i <= 5; i++) {
+  for (let j = 1; j <= 10; j++) {
+    // console.log(`${i} * ${j} = ${i * j}`);
+  }
+}
+// or
+for (let i = 1; i <= 10; i++) {
+  // console.log(`5 * ${i} = ${5 * i} `);
+}
+
+// Count how many numbers between 1 and 50 are divisible by 3.
+let count = 0;
+for (let i = 1; i <= 50; i++) {
+  if (i % 3 === 0) {
+    count++;
+  }
+}
+console.log(count);
+
+// Print reverse counting from 10 to 1.
+for (let i = 10; i >= 1; i--) {
+console.log(i)
 }
