ef55c23815a067143b5ef3a64844fee864b0e381
zariikhan <zaryabkhan18578@gmail.com>
2026-01-07 23:40:25 +0500

    PI

---
+++ 08_Classes_and_oop/mathPI.js
@@ -0,0 +1,38 @@
+// console.log(Math.PI)
+// Math.PI = 4
+// console.log(Math.PI)
+
+const pi = Object.getOwnPropertyDescriptor(Math, "PI");
+// console.log(pi);
+
+const student = {
+  name: "zaryab",
+  address: "ouch",
+};
+console.log(Object.getOwnPropertyDescriptor(student, "name"));
+for (let key in student) {
+  console.log(`${key} : ${student[key]}`);
+}
+// const descriptor = Object.defineProperty(student, "name", {
+//   writable: false,
+//   enumerable: false,
+// });
+// console.log(descriptor);
+
+// we can also add new property
+const descriptor = Object.defineProperty(student, "year", {
+  value: "2000",
+});
+// console.log(descriptor);
+
+const desc = Object.defineProperties(student, {
+  name: {
+    writable: false,
+    enumerable: false,
+  },
+  address: {
+    writable: false,
+    enumerable: false,
+  },
+});
+console.log(desc);
