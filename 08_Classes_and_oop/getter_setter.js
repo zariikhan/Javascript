77e54af987d6897dc660c627f340933ebea7cb59
zariikhan <zaryabkhan18578@gmail.com>
2026-01-08 12:41:51 +0500

    setter & getter

--- 08_Classes_and_oop/getter_setter.js
+++ 08_Classes_and_oop/getter_setter.js
@@ -22,3 +22,47 @@ class User {
 const user1 = new User("user@user234gmail.com", 1234);
 console.log(user1.email);
 console.log(user1.passwoord);
+
+const user = {
+  firstName: "zaryab",
+  lastName: "khan",
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+console.log(user.fullName);
+
+const account = {
+  _balance: 100,
+
+  set balance(value) {
+    if (value >= 0) {
+      this._balance = value;
+    } else {
+      console.log("invalid balance");
+    }
+  },
+  get balance() {
+    return this._balance;
+  },
+};
+console.log(account.balance);
+
+class Person {
+  constructor(age) {
+    this.age = age;
+  }
+  set age(value) {
+    if (value > 0) {
+      this._age = value;
+    } else {
+      console.log("invalid age");
+    }
+  }
+  get age() {
+    return this._age;
+  }
+}
+const ali = new Person(10);
+console.log(ali.age);
